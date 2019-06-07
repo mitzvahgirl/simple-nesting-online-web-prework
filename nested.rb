c4ba0b50fbf5fcb609dac1fc3e005a24a2dd390e
Amy Fogelman <abfnj1@gmail.com>
2019-06-07 01:28:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,15 @@ def hopper
           :languages => ["C"]
         }
      }
+     
+ changing_alan[:alan_kay][:known_for]).to eq("GUI")
+    end
+  end
 
+ "#adding_to_dennis" do 
+    it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do 
+      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
+  
 
 end
 
